122a528aa326c687df5896366a45cdffc5147dad
Trish Griego <pmgriego@gmail.com>
2016-09-07 13:22:33 -0700

    After crafting a working self.all: which returns a collection of instances, representing all of the objects described in csv. Started to craft the outline of self.find(id): which returns an instance of the object where the value of the id field in csv.

--- lib/markets.rb
+++ lib/markets.rb
@@ -11,8 +11,7 @@ require_relative('../far_mar')
 
 class FarMar::Markets
   attr_reader # only id will be need to be passed & checked?
-  def initialize(id, name, address, city, county, state, zip)
-  #Each individual market has many vendors associated with it. The FarMar::Market data, in order in the CSV, consists of:
+  def initialize(id, name, address, city, county, state, zip) #Each individual market has many vendors associated with it. The FarMar::Market data, in order in the CSV, consists of:
     @id = id
     @name = name
     @address = address
@@ -21,22 +20,14 @@ class FarMar::Markets
     @state = state
     @zip = zip
   end
-  # loaded all of the information from CSV file with:
-  # how can I associate all wiht this: an array of arrays.
-  # once I figure this out - I'll be able to repeat the structure elsewhere.
-
-
   # grab market info from above & force it into self.all? or should I move it to below?
   # output: return a collection of instances, representing ALL of the objects described in the CSV
   def self.all
-    # think about semantics later trish
-
     markets = CSV.read('./support/markets.csv')
     markets_array = []
-# research .map and see if after mvp you can use map.
+    # research .map and see if after mvp you can use map.
     markets.each do |market|
       id = market[0]
-      # always going to have 2nd num zero,
       name = market[1]
       address = market[2]
       city = market[3]
@@ -46,20 +37,33 @@ class FarMar::Markets
       market = FarMar:: Markets.new(id, name, address, city, county, state, zip)
       markets_array << market
     end
-    # market_object = FarMar::Markets.new(id, name, address, city, county, state, zip)
     return markets_array
   end
 
   # self.find(id)
   # also means to repeat this functionality per class?
+  #self.find: returns an instance of the object where the value of the id field in the csv matches the passed parameter.
+  # input: id
+  # output: an instance of market object if there is a matching id in csv.
   def self.find(id)
-
+    # not sure how to use already called variable outside self.all method. so until then, going to import it again.
+    markets = CSV.read('./support/markets.csv')
+    market_match = []
+    markets.each do |market|
+      id = market[0]
+      if market[0] == id
+        market = FarMar::Markets.new(id, name, address, city, county, state, zip)
+        market_match << market
+        return market
+      else
+        puts "Market not found!"
+      end
+    end
 
   end
 # necessary to build:
   def vendors
     #vendors: returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field.
-
   end
 # Read_ids
   def read_ids
