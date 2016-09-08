fcb2d94248ad8330bc016eb3cb18617b61c6dda8
Trish Griego <pmgriego@gmail.com>
2016-09-07 19:12:02 -0700

    Went back to earlier working version. Thanks Susan! Now have 2 tests working

--- specs/markets_spec.rb
+++ specs/markets_spec.rb
@@ -2,26 +2,28 @@
 require_relative 'spec_helper'
 require_relative '../far_mar'
 
-describe FarMar::Markets do
-  let(:market1) { FarMar::Market.new("Trish Stand", true) }
-  let(:market2) { FarMar::Market.new("Jon Snows", false) }
+# describe FarMar::Markets do
+#   let(:market1) { FarMar::Market.new("Trish Stand", true) }
+#   let(:market2) { FarMar::Market.new("Jon Snows", false) }
 # modeling after let lesson
 
-##### How should I break this down as far as test coverage. Do I need to seperate out the two methods of written. all & find?? #####
+##### How should I break this down as far as test coverage. Do I need to seperate out per method?
 
-
-  it "Dose market have a name?" do
-    market1.name.must_equal("Trish Stand")
-    market2.name.must_equal("Jon Snows")
+describe FarMar::Markets do
+#1
+  it "Dose .all method work??" do
+    markets = FarMar::Markets.all.length
+    expect(markets).must_equal(500)
+  end
+#2
+  it "Does .find method work??" do
+    market1 = FarMar::Markets.find(494).name
+    expect(market1).must_equal("Northside Farmers' Market - Youngstown")
   end
 
-  it "Does .all return all market objects" do
-
-  end
-
-  it "" do
-
-  end
+  # it "" do
+  #
+  # end
 
 
 
