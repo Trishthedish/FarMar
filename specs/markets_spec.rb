ae463a9f527257e8c02347ae6de90a3f9fba5452
Trish Griego <pmgriego@gmail.com>
2016-09-08 12:55:07 -0700

    Holy crap, all humble little tests are working as predicted & passing. Committing before I have a chance to break it.

--- specs/markets_spec.rb
+++ specs/markets_spec.rb
@@ -1,44 +1,13 @@
 # markets_spec.rb
 require_relative 'spec_helper'
 require_relative '../far_mar'
-
-# <<<<<<< HEAD
-# # Able to get let to work. Though I'm not comfortable with using it as effortlessly. Thus, returning to old method. Get it to work. Then I'll refactor if time allows.
-# # describe FarMar::Markets do
-# # modeling after let lesson
-# # let(:market1) { FarMar::Market.new("Trish Stand", true) }
-# # let(:market2) { FarMar::Market.new("Jon Snows", false) }
-# # modeling after let lesson
-#
-#
-# # id, name, address, city, county, state, zip)
+# Able to get let to work. Though I'm not quite comfortable with using it as effortlessly. Thus, returning to old method. Get it to work. Then I'll refactor if time allows.
 # describe FarMar::Markets do
-#
-# # 1 - This was working!! Then it wasn't ...
-# # Testing that .all method returns all 500 objects.
-#   it "does .all method work??" do
-#     markets = FarMar::Markets.all.length
-#     expect(markets).must_equal(500)
-#   end
-# # 2 - This was working!! Then it wasn't ...
-#   it "does .find method work??" do
-#     market1 = FarMar::Markets.find(494).name
-#     expect(market1).must_equal("Northside Farmers' Market - Youngstown")
-#     # 494,Northside Farmers' Market - Youngstown,1105 Elm Street,Youngstown,Mahoning,Ohio,44505
-#     # # end
-#   end
-# # 3 - This wasn't working and still isnt!!
-#   it "Does calling one instance in the market class return the market I'm looking for?" do
-#     market = FarMar::Markets.all[0].id
-#     expect(market).must_equal(1)
-#   end
-# =======
-# describe FarMar::Markets do
-#   let(:market1) { FarMar::Market.new("Trish Stand", true) }
-#   let(:market2) { FarMar::Market.new("Jon Snows", false) }
 # modeling after let lesson
-
-##### How should I break this down as far as test coverage. Do I need to seperate out per method?
+# let(:market1) { FarMar::Market.new("Trish Stand", true) }
+# let(:market2) { FarMar::Market.new("Jon Snows", false)
+# modeling after let lesson
+# id, name, address, city, county, state, zip)
 
 describe FarMar::Markets do
 #1
@@ -50,12 +19,28 @@ describe FarMar::Markets do
   it "Does .find method work??" do
     market1 = FarMar::Markets.find(494).name
     expect(market1).must_equal("Northside Farmers' Market - Youngstown")
+# 494,Northside Farmers' Market - Youngstown,1105 Elm Street,Youngstown,Mahoning,Ohio,44505
   end
-
+#3
+# Told that I should test the first & last items.
+    it "Does calling one instance in the market class return the market I'm looking for @[0][0]?" do
+      market = FarMar::Markets.all[0].id
+      expect(market).must_equal("1")
+    end
+#4 # testing last object instance...
+  it "Does the last item in the list eqaul the market that it's supposed to be?" do
+      market = FarMar::Markets.all.last.name
+      expect(market).must_equal("Montefiore Medical Center Farmers Market_Thursday")
+    end
 
 
 
 end
+# trying to get to end of document...
+# all[all.length]
+
+
+# Not as clear to me what test should be written as it was to test things in scrabble.
 
 # How do I grab a market name?
 
