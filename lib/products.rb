f4bb3ad0a630a7cc68eaabf17e13abe03d2be7ad
Trish Griego <pmgriego@gmail.com>
2016-09-09 15:16:25 -0700

    Worked on creating final methods for Product Class

--- lib/products.rb
+++ lib/products.rb
@@ -1,8 +1,12 @@
 # lib/products.rb
 require_relative('../far_mar')
+require_relative('./markets.rb')
+require_relative('./sales.rb')
+require_relative('./vendors.rb')
 
 class FarMar::Products
   # Your code goes here
+
   attr_reader :id, :name, :vendor_id
   def initialize(id, name, vendor_id)
   # Each product belongs to a vendor. The vendor_id field refers to the FarMar::Vendor ID field. The FarMar::Product data, in order in the CSV, consists of:
@@ -23,7 +27,7 @@ class FarMar::Products
     end
     return products_array
   end
-  
+
   # input: id * not be confused with market id. Which will come later.
   # output: an instance of market object if there is a matching id in csv.
   def self.find(id)
@@ -36,7 +40,62 @@ class FarMar::Products
     end
     puts "invalid, entry"
   end
+
+  def vendor
+    #vendor: returns the FarMar::Vendor instance that is associated with this vendor using the FarMar::Product vendor_id field
+    vendor_id = self.vendor_id
+    vendor = FarMar::Vendors.find(vendor_id)
+    return vendor
+  end
+
+  def sales
+    #sales: returns a collection of FarMar::Sale instances that are associated using the FarMar::Sale product_id field.
+    sales = FarMar::Sales.all
+    sales_array = []
+    sales.each do |sale|
+      if sale.product_id == id
+        sales_array << sale
+      end
+    end
+    return sales_array
+  end
+
+  def number_of_sales
+    #number_of_sales: returns the number of times this product has been sold.
+    n_sales = FarMar::Sales.all
+    n_sales_array = []
+    n_sales.each do |item|
+      if item.product_id == id
+        n_sales_array << item
+      end
+    end
+    return n_sales_array.count
+  end
+
+  def self.by_vendor(vendor_id)
+    # self.by_vendor(vendor_id): returns all of the products with the given vendor_id
+    products = FarMar::Product.all
+    by_vendor_array = []
+    products.each do |vendor|
+      if vendor.vendor_id == vendor_id.to_s
+        by_vendor_array << vendor
+      end
+    end
+    return by_vendor_array
+  end
 end
+
+
+
+
+
+# Testing new Vendor Method.
+# x = FarMar::Products.all[5].vendor.name
+# puts x
+# output: Kris and Sons
+# y = FarMar::Products.all[8].vendor.name
+# puts y
+# output: Reynolds, Schmitt and Klocko
 
 
 #tests so that I can write my tests:
--- lib/vendors.rb
+++ lib/vendors.rb
@@ -1,12 +1,10 @@
 # lib/vendors.rb
-# require_relative('./markets.rb')
-# require_relative('../lib/products.rb')
-# require_relative('../lib/sales.rb')
+
+
 require_relative('../far_mar')
-# require_relative('./markets.rb')
-# require_relative('./markets.rb')
-# require_relative('./products.rb')
-# require_relative('./sales.rb')
+require_relative('./markets.rb')
+require_relative('./products.rb')
+require_relative('./sales.rb')
 
 
 # ruby lib/vendors.rb
@@ -55,14 +53,14 @@ class FarMar::Vendors
     market_id = self.market_id
     market = FarMar::Markets.find(market_id)
     return market
-    # self.market_id
-    #the market_id of vendor.
   end
 
   def products
-    #products: returns a collection of FarMar::Product instances that are associated by the FarMar::Product vendor_id field.
+    #products: returns a collection of FarMar::Product
+    #instances that are associated by the
+    #FarMar::Product vendor_id field.
     products = FarMar::Products.all
-      products_array = []
+    products_array = []
       products.each do |product|
         if product.vendor_id == id
           products_array << product
@@ -85,22 +83,62 @@ class FarMar::Vendors
 
   def revenue
     #revenue: returns the the sum of all of the vendor's sales (in cents)
+    # not sure if I accounted for cents. But I know its adding it together.
+    #the data may be mixed.
     all_sales = FarMar::Sales.all
-    revenue_array = []
+    revenue = 0
     all_sales.each do |sale|
-
+      if sale.amount != nil
+        revenue += sale.amount.to_i
+      end
     end
-
+    return revenue
   end
-
-
-
-
+  def self.by_market(market_id)
+    # self.by_market(market_id): returns all of the vendors with the given market_id
+    vendors = FarMar::Vendors.all
+    by_market_array = []
+    vendors.each do |vendor|
+      if vendor.market_id == market_id.to_s
+        by_market_array << vendor
+      end
+    end
+    return by_market_array
+  end
 end
 
-vendor = FarMar::Vendors.all[1]
-puts vendor.market
+# this_is_crazy = FarMar::Vendors.by_market(2)
+# puts this_is_crazy
+
+
+
+
+# id, name, num_of_employees, market_id
+
+# test3 = FarMar::Vendors.find(10).revenue
+# test3 = FarMar::Vendors.find(10).revenue
+# puts test3
+
+
+
+# works for sales
+# test = FarMar::Vendors.find(10).sales
+# puts test
+
+
+#products work
+# test = FarMar::Vendors.find(10)
+#
+# puts test.products
+
+
+# x = FarMar::Vendors.all[0].revenue
+# puts x
+
 # 1,Feil-Farrell,8,1
+
+
+
 
 
 # this works!! AND Holy crap it also helped me figure out how many vendors existed in the big ass file!!
